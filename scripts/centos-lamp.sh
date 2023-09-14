9bbd4416d068c7a2e582787014889adfc5f37ba8
salihceneli <salih@ceneli.com>
2023-09-14 21:16:49 +0300

    adding files for LAMP stack

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# update CentOS with any patches
+yum update -y --exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+systemctl start httpd
+systemctl enable httpd
+systemctl stop httpd
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+systemctl start httpd
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mariadb mariadb-server mariadb-devel
+systemctl start mariadb 
+systemctl enable mariadb.service
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+
+systemctl restart httpd.service
+
--- scripts/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install Git, nano and mc
-yum install -y git
-yum install -y nano
-yum install -y mc
